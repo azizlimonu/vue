7ff1fb9ab126000fc099e512cbd3931e79a88e62
Aziz Limonu <azizlimonu@gmail.com>
2023-12-11 09:30:25 +0800

    counter ok.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+let count = ref(0);
+</script>
+
+<template>
+  <main>
+    <container class="container">
+      <h4>The current Count: </h4>
+      <h1>{{ count }}</h1>
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+    </container>
+  </main>
+</template>
